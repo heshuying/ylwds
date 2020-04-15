68d27802a877ecaa4749a091d707837af7197afe
hesy <heshuying@haier.com>
2020-04-15 17:58:36 +0800

    商品表结构变更

--- src/main/java/ltd/newbee/mall/entity/NewBeeMallGoods.java
+++ src/main/java/ltd/newbee/mall/entity/NewBeeMallGoods.java
@@ -2,6 +2,7 @@ package ltd.newbee.mall.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class NewBeeMallGoods {
@@ -17,19 +18,43 @@ public class NewBeeMallGoods {
 
     private String goodsCarousel;
 
-    private Integer originalPrice;
+    private BigDecimal originalPrice;
 
-    private Integer sellingPrice;
-    private Integer  profit;
-    private Integer  price;
+    private BigDecimal sellingPrice;
+    private BigDecimal  profit;
+    private BigDecimal  price;
     private Integer stockNum;
 
     private String tag;
     private String createSno;
     private String goodsSno;
-    private Byte goodsSellStatus;
+    /**
+     * 商品状态：1 仓库中 2 审核中待上架 3 销售中 4 销售中申请下架 5 销售中申请下架已前端下架 6 已下架仓库中
+     */
+    private Integer goodsSellStatus;
 
     private Integer createUser;
+    /**
+     * 运费
+     */
+    private BigDecimal transitMoney;
+    /**
+     * 销量
+     */
+    private Integer saleTotal;
+    /**
+     * 可选规格：多个以逗号分隔
+     */
+    private String gooodsAttribute;
+    /**
+     * 下架原因
+     */
+    private String msgOffline;
+
+    /**
+     * 上架驳回原因
+     */
+    private String msgReject;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
@@ -38,6 +63,10 @@ public class NewBeeMallGoods {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
+    /**
+     * 上架时间
+     */
+    private Date onlineTime;
 
     private String goodsDetailContent;
 
@@ -89,22 +118,6 @@ public class NewBeeMallGoods {
         this.goodsCarousel = goodsCarousel == null ? null : goodsCarousel.trim();
     }
 
-    public Integer getOriginalPrice() {
-        return originalPrice;
-    }
-
-    public void setOriginalPrice(Integer originalPrice) {
-        this.originalPrice = originalPrice;
-    }
-
-    public Integer getSellingPrice() {
-        return sellingPrice;
-    }
-
-    public void setSellingPrice(Integer sellingPrice) {
-        this.sellingPrice = sellingPrice;
-    }
-
     public Integer getStockNum() {
         return stockNum;
     }
@@ -119,14 +132,6 @@ public class NewBeeMallGoods {
 
     public void setTag(String tag) {
         this.tag = tag == null ? null : tag.trim();
-    }
-
-    public Byte getGoodsSellStatus() {
-        return goodsSellStatus;
-    }
-
-    public void setGoodsSellStatus(Byte goodsSellStatus) {
-        this.goodsSellStatus = goodsSellStatus;
     }
 
     public Integer getCreateUser() {
@@ -171,47 +176,34 @@ public class NewBeeMallGoods {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", goodsId=").append(goodsId);
-        sb.append(", goodsName=").append(goodsName);
-        sb.append(", goodsIntro=").append(goodsIntro);
-        sb.append(", goodsCoverImg=").append(goodsCoverImg);
-        sb.append(", goodsCarousel=").append(goodsCarousel);
-        sb.append(", originalPrice=").append(originalPrice);
-        sb.append(", sellingPrice=").append(sellingPrice);
-        sb.append(", stockNum=").append(stockNum);
-        sb.append(", profit=").append(profit);
-        sb.append(", price=").append(price);
-        sb.append(", tag=").append(tag);
-        sb.append(", goodsSno=").append(goodsSno);
-        sb.append(", createSno=").append(createSno);
-        sb.append(", goodsSellStatus=").append(goodsSellStatus);
-        sb.append(", createUser=").append(createUser);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateUser=").append(updateUser);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", goodsDetailContent=").append(goodsDetailContent);
-        sb.append("]");
-        return sb.toString();
-    }
-
-    public Integer getProfit() {
-        return profit;
-    }
-
-    public void setProfit(Integer profit) {
-        this.profit = profit;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
+        return "NewBeeMallGoods{" +
+                "goodsId=" + goodsId +
+                ", goodsName='" + goodsName + '\'' +
+                ", goodsIntro='" + goodsIntro + '\'' +
+                ", goodsCategoryId=" + goodsCategoryId +
+                ", goodsCoverImg='" + goodsCoverImg + '\'' +
+                ", goodsCarousel='" + goodsCarousel + '\'' +
+                ", originalPrice=" + originalPrice +
+                ", sellingPrice=" + sellingPrice +
+                ", profit=" + profit +
+                ", price=" + price +
+                ", stockNum=" + stockNum +
+                ", tag='" + tag + '\'' +
+                ", createSno='" + createSno + '\'' +
+                ", goodsSno='" + goodsSno + '\'' +
+                ", goodsSellStatus=" + goodsSellStatus +
+                ", createUser=" + createUser +
+                ", transitMoney=" + transitMoney +
+                ", saleTotal=" + saleTotal +
+                ", gooodsAttribute='" + gooodsAttribute + '\'' +
+                ", msgOffline='" + msgOffline + '\'' +
+                ", msgReject='" + msgReject + '\'' +
+                ", createTime=" + createTime +
+                ", updateUser=" + updateUser +
+                ", updateTime=" + updateTime +
+                ", onlineTime=" + onlineTime +
+                ", goodsDetailContent='" + goodsDetailContent + '\'' +
+                '}';
     }
 
     public String getCreateSno() {
@@ -228,5 +220,93 @@ public class NewBeeMallGoods {
 
     public void setGoodsSno(String goodsSno) {
         this.goodsSno = goodsSno;
+    }
+
+    public void setGoodsSellStatus(Integer goodsSellStatus) {
+        this.goodsSellStatus = goodsSellStatus;
+    }
+
+    public BigDecimal getTransitMoney() {
+        return transitMoney;
+    }
+
+    public void setTransitMoney(BigDecimal transitMoney) {
+        this.transitMoney = transitMoney;
+    }
+
+    public Integer getSaleTotal() {
+        return saleTotal;
+    }
+
+    public void setSaleTotal(Integer saleTotal) {
+        this.saleTotal = saleTotal;
+    }
+
+    public String getGooodsAttribute() {
+        return gooodsAttribute;
+    }
+
+    public void setGooodsAttribute(String gooodsAttribute) {
+        this.gooodsAttribute = gooodsAttribute;
+    }
+
+    public String getMsgOffline() {
+        return msgOffline;
+    }
+
+    public void setMsgOffline(String msgOffline) {
+        this.msgOffline = msgOffline;
+    }
+
+    public String getMsgReject() {
+        return msgReject;
+    }
+
+    public void setMsgReject(String msgReject) {
+        this.msgReject = msgReject;
+    }
+
+    public Date getOnlineTime() {
+        return onlineTime;
+    }
+
+    public void setOnlineTime(Date onlineTime) {
+        this.onlineTime = onlineTime;
+    }
+
+    public BigDecimal getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(BigDecimal originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public BigDecimal getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(BigDecimal sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public BigDecimal getProfit() {
+        return profit;
+    }
+
+    public void setProfit(BigDecimal profit) {
+        this.profit = profit;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getGoodsSellStatus() {
+        return goodsSellStatus;
     }
 }

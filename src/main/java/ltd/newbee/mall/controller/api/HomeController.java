55c5989000b9d7fe28fd9d9a914cc5d7a127e9d4
19012964 <panyulong@haier.com>
2020-04-28 15:18:00 +0800

    集成swagger

--- src/main/java/ltd/newbee/mall/controller/api/HomeController.java
+++ src/main/java/ltd/newbee/mall/controller/api/HomeController.java
@@ -4,11 +4,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import ltd.newbee.mall.dto.ProfileDto;
 import ltd.newbee.mall.dto.RegisterFirstDto;
-import ltd.newbee.mall.entity.TbUserProfile;
 import ltd.newbee.mall.service.TbModuleService;
 import ltd.newbee.mall.service.TbUserService;
 import ltd.newbee.mall.util.Result;
-import ltd.newbee.mall.util.ResultGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,35 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by 19012964 on 2020/4/15.
  */
-@Api(value = "用户管理相关接口", tags = {"用户管理"})
+@Api(value = "首页相关接口", tags = {"首页"})
 @RestController
 @RequestMapping("/api")
-public class AuthController {
+public class HomeController {
 
     @Autowired
     private TbUserService userService;
+    @Autowired
+    private TbModuleService moduleService;
 
-    @ApiOperation(value = "注册")
-    @PostMapping("/register/first")
+    @ApiOperation(value = "获取banner图")
+    @GetMapping("/banners")
     @ResponseBody
-    public Result registFirst(@RequestBody RegisterFirstDto registerFirstDto){
-
-        return userService.register(registerFirstDto);
+    public Result banners(){
+        return moduleService.getBanner();
     }
 
-    @ApiOperation(value = "获取用户注册信息")
-    @GetMapping("/profile/info")
+    @ApiOperation(value = "获取专区")
+    @GetMapping("/modules")
     @ResponseBody
-    public Result profileInfo(@RequestParam Long userId){
-        return userService.getProfile(userId);
+    public Result modules(){
+        return moduleService.getModules();
     }
-
-    @ApiOperation(value = "用户注册信息更新")
-    @PostMapping("/profile/update")
-    @ResponseBody
-    public Result updateProfile(@RequestBody ProfileDto dto){
-        return userService.updateUserProfile(dto);
-    }
-
 
 }

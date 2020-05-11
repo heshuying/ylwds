77ad5723f8e2709080c8e80a17719cb08e525d55
19012964 <panyulong@haier.com>
2020-05-11 10:18:45 +0800

    去掉无用的代码

--- src/main/resources/ylw_db.sql
+++ src/main/resources/ylw_db.sql
@@ -12,12 +12,12 @@ CREATE TABLE `tb_goods_category`  (
   `category_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '分类名称',
   `category_rank` int(11) NOT NULL DEFAULT 0 COMMENT '排序值(字段越大越靠前)',
   `is_deleted` tinyint(4) NOT NULL DEFAULT 0 COMMENT '删除标识字段(0-未删除 1-已删除)',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `create_user` int(11) NOT NULL DEFAULT 0 COMMENT '创建者id',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   `update_user` int(11) NULL DEFAULT 0 COMMENT '修改者id',
   PRIMARY KEY (`category_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 107 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 INSERT INTO `tb_goods_category` VALUES (15, 1, 0, '家电 数码 手机', 100, 0, '2019-09-11 18:45:40', 0, '2019-09-11 18:45:40', 0);
 INSERT INTO `tb_goods_category` VALUES (16, 1, 0, '女装 男装 穿搭', 99, 0, '2019-09-11 18:46:07', 0, '2019-09-11 18:46:07', 0);
@@ -47,11 +47,11 @@ CREATE TABLE `tb_goods_info`  (
   `goods_sell_status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '商品上架状态 0-下架 1-上架',
   `create_sno` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '资源方编码',
   `create_user` int(11) NOT NULL DEFAULT 0 COMMENT '添加者主键id',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '商品添加时间',
+  `create_time` datetime DEFAULT NULL COMMENT '商品添加时间',
   `update_user` int(11) NOT NULL DEFAULT 0 COMMENT '修改者主键id',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '商品修改时间',
+  `update_time` datetime DEFAULT NULL COMMENT '商品修改时间',
   PRIMARY KEY (`goods_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10896 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- 专区表
@@ -59,14 +59,14 @@ CREATE TABLE `tb_goods_info`  (
 DROP TABLE IF EXISTS `tb_module`;
 CREATE TABLE `tb_module`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `mod_key` varchar(50) not null default '' comment '专区编码'
+  `mod_key` varchar(50) not null default '' comment '专区编码',
   `mod_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '专区名称',
   `mod_desc` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '专区描述',
   `mod_rank` int(11) NOT NULL DEFAULT 0 COMMENT '排序值(字段越大越靠前)',
   `is_deleted` tinyint(4) NOT NULL DEFAULT 0 COMMENT '删除标识字段(0-未删除 1-已删除)',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `create_user` int(11) NOT NULL DEFAULT 0 COMMENT '创建者id',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   `update_user` int(11) NULL DEFAULT 0 COMMENT '修改者id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -85,9 +85,9 @@ CREATE TABLE `tb_module_detail`  (
   `is_deleted` tinyint(4) NOT NULL DEFAULT 0 COMMENT '删除标识字段(0-未删除 1-已删除)',
   `desc1` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '推荐理由1',
   `desc2` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '推荐理由2',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `create_user` int(11) NOT NULL DEFAULT 0 COMMENT '创建者id',
-  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   `update_user` int(11) NULL DEFAULT 0 COMMENT '修改者id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -135,15 +135,15 @@ CREATE TABLE `tb_order_orderinfo` (
 DROP TABLE IF EXISTS `tb_settle`;
 CREATE TABLE `tb_settle` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '结算表id',
-  `status` int(11) DEFAULT NULL COMMENT '订单状态：01-待确认；02-待平台更改；03-待开票   04-待付款   05-已完结',  
+  `status` int(11) DEFAULT NULL COMMENT '订单状态：01-待确认；02-待平台更改；03-待开票   04-待付款   05-已完结',
   `settle_no` varchar(50)  NOT NULL DEFAULT '' COMMENT '结算单号',
   `settle_name` varchar(80)  NOT NULL DEFAULT '' COMMENT '结算单名称',
   `begin_period` varchar(50)  NOT NULL DEFAULT '' COMMENT '结算开始时间',
   `end_period` varchar(50)  NOT NULL DEFAULT '' COMMENT '结算开始时间',
   `amount` decimal(10,2)  NOT NULL DEFAULT 0 COMMENT '结算金额',
   `supplier_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '供货商的id',
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='结算表';
 
@@ -158,7 +158,7 @@ CREATE TABLE `tb_settle_detail` (
   `settle_price` decimal(10,2) NOT NULL DEFAULT 0 COMMENT '结算单价',
   `settle_num` int(11)  NOT NULL DEFAULT 0 COMMENT '结算数量',
   `amount` decimal(10,2)  NOT NULL DEFAULT 0 COMMENT '结算金额',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='结算明细表';
 
@@ -172,7 +172,7 @@ CREATE TABLE `tb_shopping_cart`  (
   `goods_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '关联商品id',
   `goods_count` int(11) NOT NULL DEFAULT 0 COMMENT '数量',
   `goods_attr` varchar(80) not null default '' comment '规格',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -188,12 +188,12 @@ CREATE TABLE `tb_user`  (
   `introduce_sign` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '个性签名',
   `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '收货地址',
   `cellphone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
-  `user_type` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '01:个人客户;02:商户；03平台；04：供应商', 
+  `user_type` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '01:个人客户;02:商户；03平台；04：供应商',
   `user_status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0-待审核 1-正常 2-锁定；3-审核不通过',
   `check_msg` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '审核备注',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+  `create_time` datetime DEFAULT NULL COMMENT '注册时间',
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- 用户信息
@@ -222,7 +222,7 @@ CREATE TABLE `tb_user_profile`  (
   `bank_name` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '开户行名称',
   `bank_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '开户行代码',
   `promise_uri` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '供应商承诺书',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`profile_id`) USING BTREE,
   index idx_profile_user(user_id) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -245,7 +245,7 @@ CREATE TABLE `tb_user_addr`  (
   `post_code` varchar(50)  NOT NULL DEFAULT '' COMMENT '邮编',
   `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态',
   `is_default` tinyint(1) NOT NULL DEFAULT 0 COMMENT '默认 1；非默认 0',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`addr_id`) USING BTREE,
   index idx_addr_user(user_id) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -260,12 +260,12 @@ CREATE TABLE `tb_user_oper`  (
   `user_id` bigint(20) NOT NULL default 0 COMMENT '用户主键id',
   `goods_id` bigint(20) NOT NULL default 0 COMMENT '产品ID',
   `oper` varchar(50) NOT NULL DEFAULT '' COMMENT 'Fav; Click',
-  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   index idx_addr_user(user_id) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-alter table tb_order_goodinfo add goods_attr varchar(80) not null default '' comment '规格'
-alter table tb_order_orderinfo add delivery_id bigint(20) not null default 0 comment '发货地址ID'
-alter table tb_order_orderinfo add pay_type varchar(20) not null default '' comment '支付方式'
-alter table tb_order_orderinfo add delivery_fee decimal(5,2) NOT NULL DEFAULT 0 comment '运费'
+alter table tb_order_goodinfo add goods_attr varchar(80) not null default '' comment '规格';
+alter table tb_order_orderinfo add delivery_id bigint(20) not null default 0 comment '发货地址ID';
+alter table tb_order_orderinfo add pay_type varchar(20) not null default '' comment '支付方式';
+alter table tb_order_orderinfo add delivery_fee decimal(5,2) NOT NULL DEFAULT 0 comment '运费';

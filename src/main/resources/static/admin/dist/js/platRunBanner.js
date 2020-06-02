f49bd888948f6ba342748b894a69117e064df565
gounk/19030044 <gounaikang@haier.com>
2020-06-02 09:09:59 +0800

    运营管理--轮播图页面部分代码提交

--- src/main/resources/static/admin/dist/js/platRunBanner.js
+++ src/main/resources/static/admin/dist/js/platRunBanner.js
@@ -2,18 +2,26 @@ new Vue({
     el: '#app',
     data() {
         return {
+            baseUrl: '',
             tableData: [{
                 img: 'http://localhost:8080/upload/20200527/3b5421fdd24348908d58ccc92a371fe0.png',
+                imgOri: '/upload/20200527/3b5421fdd24348908d58ccc92a371fe0.png',
                 url: 'https://lanhuapp.com/web/#/item/project/board?pid=292c8f-5942-43cd-a335-f8b43c95',
                 order: '12',
                 createDate: '2020/03/03'
             }],
-            bannerCount: 25,
             visible: false,
-            showBtnWord: '收起'
+            showBtnWord: '收起',
+            moralTitle: '新增轮播',
+            moralShow: false,
+            closeBtnShow: false,
+            wholeUrl: '',
+            jumpUrl: '',
+            sortNum: ''
         };
     },
     mounted() {
+        this.baseUrl = window.localStorage.getItem("baseUrl");
         this.visible = true;
     },
     methods: {
@@ -26,6 +34,38 @@ new Vue({
                 this.showBtnWord = '收起';
             }else {
                 this.showBtnWord = '展示位置';
+            }
+        },
+        // 上传成功
+        handleUploadSuccess(res) {
+            if (res.resultCode == "200") {
+                this.$message.success('上传成功');
+                this.wholeUrl = this.baseUrl + res.data;
+            } else {
+                this.$message.error("上传失败，" + res.msg);
+            }
+        },
+        // 新增banner
+        addBanner() {
+            this.moralTitle = '新增轮播';
+            this.moralShow = true;
+            this.wholeUrl = '';
+            this.jumpUrl = '';
+            this.sortNum = '';
+        },
+        // 修改banner
+        editBanner(item) {
+            this.moralTitle = '修改轮播';
+            this.moralShow = true;
+            this.wholeUrl = this.baseUrl + item.imgOri;
+            this.jumpUrl = item.url;
+            this.sortNum = item.order;
+        },
+        // 保存banner
+        saveBanner() {
+            if(this.wholeUrl === '' || this.jumpUrl === '' || this.sortNum === '') {
+                this.$message.error('请填写完整信息后重试!');
+                return;
             }
         }
     }
